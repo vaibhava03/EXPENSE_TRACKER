52a987ccda97b24c5ecdcbc430d607292ee366b2
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-03-29 14:01:27 +0530

    Add files via upload

---
+++ backend/src/expenses.ts
@@ -0,0 +1,20 @@
+const path=require('path');
+
+const express=require('express');
+const router=express.Router();
+const ExpenseController = require('../controllers/expenses')
+
+router.post('/user/expense', ExpenseController.postExpenses);
+router.post('/user/pay',ExpenseController.postPay);
+
+
+router.get('/user/leaderboard', ExpenseController.getLeaderBoard);
+router.get('/user/periodical-expenses', ExpenseController.getPeriodicalExpenses);
+
+
+router.get('/download',ExpenseController.getExpensesD);
+
+router.get('/user/prev-files', ExpenseController.getPrevFiles)
+ 
+
+export default router;
---
+++ backend/src/login.ts
@@ -0,0 +1,16 @@
+const path=require('path');
+
+const express=require('express');
+const router=express.Router();
+const LoginController =require('../controllers/login.js');
+
+router.post('/user/signup', LoginController.postSignup);
+router.post('/user/login',LoginController.postLogin);
+
+router.post('/user/password/forgotpassword',LoginController.postForgetPassword);
+router.use('/password/resetpassword/:passwordId',LoginController.useResetPassword);
+router.use('/updatepassword/:resetpasswordid',LoginController.useUpdatePassword);
+
+
+
+export default router;
