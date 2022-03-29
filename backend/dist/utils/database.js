4c19c7dcb8e03a540e7ea24ff5bdfbc3156d1921
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-03-29 14:19:05 +0530

    Update database.js

--- backend/dist/utils/database.js
+++ backend/dist/utils/database.js
@@ -1,9 +1,8 @@
-const Sequelize= require('sequelize');
-
-const sequelize=new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    dialect:'mysql', 
-    host:process.env.DB_HOST,
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
 });
-module.exports=sequelize;
-
-
+exports.default = sequelize;
