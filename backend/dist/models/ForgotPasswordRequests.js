f005ef49d5d098bb2cc8a53baba2f3ff275976d1
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-03-29 14:22:04 +0530

    Update ForgotPasswordRequests.js

--- backend/dist/models/ForgotPasswordRequests.js
+++ backend/dist/models/ForgotPasswordRequests.js
@@ -1,13 +1,16 @@
-const Sequelize=require('sequelize');
-const sequelize=require('../util/database');
-
-const ForgetPassWordRequests=sequelize.define('FPReq',{
-    id:{
-        type:Sequelize.STRING,
-        primaryKey:true,
-        allowNull:false
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = __importDefault(require("sequelize"));
+const database_1 = __importDefault(require("../util/database"));
+const ForgetPassWordRequests = database_1.default.define('FPReq', {
+    id: {
+        type: sequelize_1.default.STRING,
+        primaryKey: true,
+        allowNull: false
     },
-    isActive:Sequelize.BOOLEAN
-
-})
-module.exports=ForgetPassWordRequests;
+    isActive: sequelize_1.default.BOOLEAN
+});
+exports.default = ForgetPassWordRequests;
