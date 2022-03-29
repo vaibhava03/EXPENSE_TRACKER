8a104432734cf905d0c4121411ec59a14617b403
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-03-29 14:24:24 +0530

    Update testExpenseTracker.ts

--- backend/src/testExpenseTracker.ts
+++ backend/src/testExpenseTracker.ts
@@ -1,6 +1,7 @@
-   import path from 'path';
+ import path from 'path';
+ import fs from 'fs';
    import express from 'express';
-      const app=express();
+   const app=express();
    import dotenv from 'dotenv';
    dotenv.config({ path: path.resolve(__dirname, 'util/.env') })
    import bodyParser from 'body-parser';
@@ -11,19 +12,23 @@
    import FPReq from './models/ForgotPasswordRequests';
    import FileUrl from './models/FileUrl';
    import cors from 'cors';
+   import helmet from 'helmet';
+   import morgan from 'morgan';
+
+   app.use(helmet());
    app.use(cors());
+
    app.use(bodyParser.urlencoded({extended:false}));
    app.use(bodyParser.json());
+   const accessLogStream=fs.createWriteStream(path.join(__dirname, 'access.log'), {flags:'a'});
+   app.use(morgan('combined', {stream:accessLogStream}));
 
    import loginRoutes from './routes/login';
    import expensesRoutes from './routes/expenses';
+
    app.use(loginRoutes);
    app.use(expensesRoutes);
  
-   
-
-   
-
 
    User.hasMany(Expense);
    Expense.belongsTo(User);
@@ -36,9 +41,9 @@
    sequelize
     .sync()
     .then(() =>{
-    }).catch((Error: any)  => {
-      console.log('ERROR:', Error.message);
-  });
+    }).catch(function (Error: any): void {
+          console.log('ERROR:', Error);
+       });
 
   
 
