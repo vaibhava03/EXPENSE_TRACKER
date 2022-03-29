6503ef461981a0f3e5bcef04879f5b25614e2973
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-03-29 14:22:24 +0530

    Update order.js

--- backend/dist/models/order.js
+++ backend/dist/models/order.js
@@ -1,13 +1,17 @@
-const Sequelize=require('sequelize');
-const sequelize=require('../util/database');
-
-const Order=sequelize.define('order',{
-    id:{
-        type:Sequelize.INTEGER,
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = __importDefault(require("sequelize"));
+const database_1 = __importDefault(require("../util/database"));
+const Order = database_1.default.define('order', {
+    id: {
+        type: sequelize_1.default.INTEGER,
         autoIncrement: true,
         allowNull: false,
-        primaryKey:true
+        primaryKey: true
     },
-    orderid:Sequelize.STRING
-})
-module.exports=Order;
+    orderid: sequelize_1.default.STRING
+});
+exports.default = Order;
