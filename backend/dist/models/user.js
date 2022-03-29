51defdc22963186ea1989e0521d1f94df76fea14
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-03-29 14:22:45 +0530

    Update user.js

--- backend/dist/models/user.js
+++ backend/dist/models/user.js
@@ -1,26 +1,26 @@
-const Sequelize=require('sequelize');
-const sequelize=require('../util/database');
-
-const User=sequelize.define('user',{
-    id:
-    {
-        type: Sequelize.INTEGER,
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+const database_1 = __importDefault(require("../util/database"));
+const User = database_1.default.define('user', {
+    id: {
+        type: sequelize_1.INTEGER,
         autoIncrement: true,
         allowNull: false,
-        primaryKey:true
+        primaryKey: true
     },
-        name:Sequelize.STRING,
-        email:{
-           type:Sequelize.STRING,
-           unique:true
-          },
-        phone:{
-           type:Sequelize.STRING,
-           unique:true
-          },
-        password:Sequelize.STRING
-    }
-);
-
-
-module.exports=User;
+    name: sequelize_1.STRING,
+    email: {
+        type: sequelize_1.STRING,
+        unique: true
+    },
+    phone: {
+        type: sequelize_1.STRING,
+        unique: true
+    },
+    password: sequelize_1.STRING
+});
+exports.default = User;
