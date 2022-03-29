0f8e62784baab25ec0f389e4299ecbbbb36f5241
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-03-29 14:21:37 +0530

    Update FileUrl.js

--- backend/dist/models/FileUrl.js
+++ backend/dist/models/FileUrl.js
@@ -1,18 +1,17 @@
-const Sequelize=require('sequelize');
-const sequelize=require('../util/database');
-
-const Expense=sequelize.define('fileUrl',{
-    id:
-    {
-        type: Sequelize.INTEGER,
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = __importDefault(require("sequelize"));
+const database_1 = __importDefault(require("../util/database"));
+const FileUrl = database_1.default.define('fileUrl', {
+    id: {
+        type: sequelize_1.default.INTEGER,
         autoIncrement: true,
         allowNull: false,
-        primaryKey:true
+        primaryKey: true
     },
-        fileUrl:Sequelize.STRING,
-        
-    }
-);
-
-
-module.exports=Expense;
+    fileUrl: sequelize_1.default.STRING,
+});
+exports.default = FileUrl;
