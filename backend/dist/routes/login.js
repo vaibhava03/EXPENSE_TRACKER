2c092c32cbe824e4580357eade802d146807f6c3
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-03-29 14:19:35 +0530

    Update login.js

--- backend/dist/routes/login.js
+++ backend/dist/routes/login.js
@@ -1,15 +1,12 @@
-const path=require('path');
-const express=require('express');
-const router=express.Router();
-const LoginController=require('../controllers/login.js');
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const LoginController = require('../controllers/login.js');
 router.post('/user/signup', LoginController.postSignup);
-router.post('/user/login',LoginController.postLogin);
-
-router.post('/user/password/forgotpassword',LoginController.postForgetPassword);
-router.use('/password/resetpassword/:passwordId',LoginController.useResetPassword);
-router.use('/updatepassword/:resetpasswordid',LoginController.useUpdatePassword);
-
-
-
-module.exports=router;
+router.post('/user/login', LoginController.postLogin);
+router.post('/user/password/forgotpassword', LoginController.postForgetPassword);
+router.use('/password/resetpassword/:passwordId', LoginController.useResetPassword);
+router.use('/updatepassword/:resetpasswordid', LoginController.useUpdatePassword);
+exports.default = router;
